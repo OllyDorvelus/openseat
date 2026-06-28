c0d712c2b60518bc5f92524dd0c1a6cfc8a38157
ollydorvelus <ollydorvelus@gmail.com>
2026-06-28 17:39:07 -0400

    remove reviews app and create trust app

--- backend/apps/trust/apps.py
+++ backend/apps/trust/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ReviewsConfig(AppConfig):
+class TrustConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'reviews'
+    name = 'trust'
