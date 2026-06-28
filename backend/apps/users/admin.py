5c2049e83d7358181abeb0c9ae446a31fe5958b4
ollydorvelus <ollydorvelus@gmail.com>
2026-06-27 23:28:55 -0400

    define user models

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from apps.users.models import Profile, Interest, Group, GroupMembership
 # Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'bio',)
+
+@admin.register(Interest)
+class InterestAdmin(admin.ModelAdmin):
+    list_display = ('name', 'icon')
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(GroupMembership)
+class GroupMembershipAdmin(admin.ModelAdmin):
+    pass
